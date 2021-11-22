0cc3bbda5821603d084d6680aaa73e999914bc6d
masaa0802 <masaakigoto0802@gmail.com>
2021-11-22 14:47:31 +0000

    利用方法完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,13 +40,13 @@ $(".theTarget").skippr({
   // ナビゲーション矢印の表示（trueで表示）
   arrows : true,
   // スライドショーの自動再生（falseで自動再生なし）
-  autoPlay : false,
+  autoPlay : true,
   // 自動再生時のスライド切替間隔（ミリ秒）
   autoPlayDuration : 5000,
   // キーボードの矢印キーによるスライド送りの設定（trueで有効）
   keyboardOnAlways : true,
   // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
-  hidePrevious : false
+  hidePrevious : true,
 });
 
 });
