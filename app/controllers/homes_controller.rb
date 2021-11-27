e1bfd6cbef88bce503fbb1d29ebaca479520c7ed
masaa0802 <masaakigoto0802@gmail.com>
2021-11-27 06:37:13 +0000

    レスポンシブ対応

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,11 @@
 class HomesController < ApplicationController
   def top
   end
-
   def about
+    @image = HomeImage.find(1)
+    @image = HomeImage.find(2)
+    @image = HomeImage.find(3)
+    @image = HomeImage.find(4)
+    @image = HomeImage.find(5)
   end
 end
